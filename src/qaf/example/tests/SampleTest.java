877c35a25b2e3ca6cdacd1a26fa721cf6c9ac2f5
sjpatel91 <sjpatel91@gmail.com>
2020-04-30 21:45:21 -0700

    first commit

---
+++ src/qaf/example/tests/SampleTest.java
@@ -0,0 +1,18 @@
+package qaf.example.tests;
+
+import static com.qmetry.qaf.automation.step.CommonStep.get;
+import static com.qmetry.qaf.automation.step.CommonStep.verifyLinkWithPartialTextPresent;
+import static qaf.example.steps.StepsLibrary.searchFor;
+import org.testng.annotations.Test;
+
+import com.qmetry.qaf.automation.ui.WebDriverTestCase;
+
+public class SampleTest extends WebDriverTestCase {
+
+	@Test
+	public void testGoogleSearch() {
+		get("/");
+		searchFor("qaf github infostretch");
+		verifyLinkWithPartialTextPresent("qaf");
+	}
+}
