d8d0838c909da749beb43fe40972215982f87811
sjpatel91 <sjpatel91@gmail.com>
2020-05-02 14:30:42 -0700

    test cases commit

--- src/qaf/example/steps/StepsLibrary.java
+++ src/qaf/example/steps/StepsLibrary.java
@@ -1,7 +1,10 @@
 package qaf.example.steps;
 
+import static com.qmetry.qaf.automation.step.CommonStep.clear;
 import static com.qmetry.qaf.automation.step.CommonStep.click;
+import static com.qmetry.qaf.automation.step.CommonStep.mouseOver;
 import static com.qmetry.qaf.automation.step.CommonStep.sendKeys;
+import static com.qmetry.qaf.automation.step.CommonStep.submit;
 
 import com.qmetry.qaf.automation.step.QAFTestStep;
 
@@ -15,4 +18,46 @@ public class StepsLibrary {
 		sendKeys(searchTerm, "input.search");
 		click("button.search");
 	}
+	@QAFTestStep(description = "search for {0}")
+	public static void baseSection() {
+		click("recipe.search");
+		}
+	
+	@QAFTestStep(description = "search for {0}")
+	public static void recipeSearch() {
+		baseSection();
+		click("banana_recipe.search");
+		click("full_recipe.search");
+	}
+	@QAFTestStep(description = "search for {0}")
+	public static void recipeDropdownSearch() {
+		baseSection();
+		click("dropdown.search");
+		click("trending.search");
+	}
+	@QAFTestStep(description = "search for {0}")
+	public static void recipeDaySearch() {
+		mouseOver("mouse_over_recipe.search");
+		click("share.search");
+		click("share_close.search");
+		click("recipe_of_day.search");
+	}
+	
+	@QAFTestStep(description = "search for {0}")
+	public static void serveSearch() {
+		baseSection();
+		click("banana_recipe.search");
+		click("serve.search");
+		clear("serve_keys.search");
+		sendKeys("-5", "serve_keys.search");
+		click("done_serve.search");
+	}
+	@QAFTestStep(description = "search for {0}")
+	public static void searchRecipeSearch() {
+		baseSection();
+		sendKeys("%", "search_recipe.search");
+		submit("search_recipe.search");
+	}
+	
+	
 }
--- src/qaf/example/tests/SampleTest.java
+++ src/qaf/example/tests/SampleTest.java
@@ -2,17 +2,72 @@ package qaf.example.tests;
 
 import static com.qmetry.qaf.automation.step.CommonStep.get;
 import static com.qmetry.qaf.automation.step.CommonStep.verifyLinkWithPartialTextPresent;
-import static qaf.example.steps.StepsLibrary.searchFor;
+import static com.qmetry.qaf.automation.step.CommonStep.click;
+import static com.qmetry.qaf.automation.step.CommonStep.sendKeys;
+import static com.qmetry.qaf.automation.step.CommonStep.*;
+import static qaf.example.steps.StepsLibrary.*;
+
+import org.openqa.selenium.WebDriver.Navigation;
 import org.testng.annotations.Test;
 
 import com.qmetry.qaf.automation.ui.WebDriverTestCase;
+import com.thoughtworks.selenium.webdriven.commands.GetAttribute;
 
 public class SampleTest extends WebDriverTestCase {
 
-	@Test
+	/*@Test
 	public void testGoogleSearch() {
 		get("/");
 		searchFor("qaf github infostretch");
 		verifyLinkWithPartialTextPresent("qaf");
+	}*/
+	@Test
+	public void testRecipeSearch() {
+		get("https://www.food.com/");
+		recipeSearch();
+		assertAttribute("title.search", "innerHTML","Best Banana Bread");
+		assertAttribute("notes.search", "innerHTML", "MY PRIVATE NOTES");
 	}
+	
+	@Test
+	public void testRecipeDropdownSearch() {
+		get("https://www.food.com/");
+		recipeDropdownSearch();	
+		assertAttribute("trending_value.search", "innerHTML", "Trending");
+		
+	}
+	@Test
+	public void testRecipeDaySearch() {
+		get("https://www.food.com/");
+		baseSection();
+		assertAttribute("recipe_day_main_page_title.search", "innerHTML", "Mexican Meatball Soup");
+		assertNotVisible("share.search");
+		recipeDaySearch();
+		assertAttribute("recipe_day_title.search", "innerHTML", "70 Regional Mexican Foods to Make at Home");
+		
+	}
+	
+	//Negative
+	
+	
+	@Test
+	public void testServeSearch() {
+		get("https://www.food.com/");
+		serveSearch();
+		assertAttribute("serve.search", "innerHTML", "-50");
+		assertNotAttribute("ingredient.search", "innerHTML", "5");
+		
+		
+	}
+	@Test
+	public void testSearchBoxSearch() {
+		get("https://www.food.com/");
+		searchRecipeSearch();
+		assertAttribute("message_recipe.search", "innerHTML", "Bad Request");
+		
+	}
+	
+	
+	
+	
 }
